a65f4ec42bb4720929944a13b87df406efb5badd
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-12 16:57:03 +0530

    Time: 67 ms (18.26%), Space: 6.2 MB (99.94%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,19 +1,22 @@
 class Solution
 {
     public:
-        void solveSudoku(vector<vector < char>> &board) {
+        void solveSudoku(vector<vector < char>> &board)
+        {
             solve(board);
         }
+    private:
     bool isValid(vector<vector < char>> &board, int row, int col, char c)
     {
         for (int i = 0; i < 9; i++)
         {
+        	// checking the rows:-
             if (board[i][col] == c)
                 return false;
-
+           	// checking the particular column :-
             if (board[row][i] == c)
                 return false;
-
+           	// checking the particular box :- 
             if (board[3 *(row / 3) + i / 3][3 *(col / 3) + i % 3] == c)
                 return false;
         }
@@ -22,20 +25,26 @@ class Solution
 
     bool solve(vector<vector < char>> &board)
     {
+       	// iterating the rows:-
         for (int i = 0; i < board.size(); i++)
         {
+           	// iterating the col's :-
             for (int j = 0; j < board[0].size(); j++)
             {
+               	// if the cell is not solved :-
                 if (board[i][j] == '.')
                 {
+                   	// checking all the possiblities :-
                     for (char c = '1'; c <= '9'; c++)
                     {
+
                         if (isValid(board, i, j, c))
                         {
                             board[i][j] = c;
-
+                           	// recursion call further:-
                             if (solve(board))
                                 return true;
+                           	// backtracking :-
                             else
                                 board[i][j] = '.';
                         }
