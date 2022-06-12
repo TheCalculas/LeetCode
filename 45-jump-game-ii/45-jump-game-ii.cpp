f378382a9527565ad788702200aa9a60743f2c44
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-12 09:51:00 +0530

    Time: 24 ms (64.35%), Space: 16.6 MB (81.09%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,20 +1,22 @@
-class Solution {
-public:
-    int jump(vector<int>& nums) {
-        int start = 0;
-        int end = 0;
-        int count = 0;
-        while(end<nums.size()-1)
+class Solution
+{
+    public:
+        int jump(vector<int> &nums)
         {
-            int mx = start;
-            for(int i=start; i<=end; i++)
+            int start = 0;
+            int end = 0;
+            int count = 0;
+            while (end < nums.size() - 1)
             {
-                mx = max(mx, nums[i]+i);
+                int mx = start;
+                for (int i = start; i <= end; i++)
+                {
+                    mx = max(mx, nums[i] + i);
+                }
+                start = end + 1;
+                end = mx;
+                count++;
             }
-            start = end+1;
-            end = mx;
-            count++;
+            return count;
         }
-        return count;
-    }
 };
