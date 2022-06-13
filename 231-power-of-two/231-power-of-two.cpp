43d2528bd8fe2bad3ef16cdfbe080a84df7a6685
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-14 00:07:58 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.19%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,12 +1,8 @@
-class Solution {
-public:
-    bool isPowerOfTwo(int n) {
-        int count = 0;
-        for(int i=0; i<100; i++)
+class Solution
+{
+    public:
+        bool isPowerOfTwo(int n)
         {
-            count += n&1;
-            n >>= 1;
+            return n > 0 and(1 << 31) % n == 0;
         }
-        return (count==1)?true:false;
-    }
 };
