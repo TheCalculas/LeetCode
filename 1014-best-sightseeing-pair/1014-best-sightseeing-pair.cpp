4700d29a2ba010de38bdd61e51cc1f719f4cae83
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-13 13:44:22 +0530

    Time: 59 ms (85.33%), Space: 39.3 MB (85.09%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
+        // curr best will store the best values rn present 
         int currbest = values[0] + 0;
+        // ans will store the result
         int ans = 0;
+        // n is the size of the array
         int n = values.size();
         
         for(int i=1;i<n;i++)
         {
+            // as we have to find the best ans we'll maximize it
             ans = max(ans, currbest + values[i] - i);
+            // in this curr best == values[j] + j
+            // so when you perform currbest + values[i] - i
+            // it gets evaluated as => values[I] +  values[j] + j - i where ( i > j)
+            // store the current best in the current best
             currbest = max(currbest, values[i] + i);
         }
         
