aea626f04fa902c849304ea57311a3c2c03da52a
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-13 10:28:17 +0530

    Time: 11 ms (54.51%), Space: 13.6 MB (94.99%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -3,26 +3,33 @@ class Solution
     public:
         int maxProduct(vector<int> &v)
         {
+            // base case when size of the vector is 1 
             if(v.size()==1) return v[0];
+            // temp variables to store the minimum and maximum
             int mn = 1;
             int mx = 1;
             int gmax = 0;
             for (int i = 0; i < v.size(); i++)
             {
+                // as the values would change so I'm storing the prev values in these variables
                 auto maxi = mx;
                 auto mini = mn;
+                // Here I'm solving the edge case when 0 would be there
                 if (v[i] == 0)
                 {
+                    // if zero is there then taking both as zero as it disrupts
                     mx = 0;
                     mn = 0;
                 }
                 else
                 {
+                    // Here I'm computing the max and min for sake
                     mx = max(maxi *v[i], mini *v[i]);
                     mx = max(mx, v[i]);
                     mn = min(maxi *v[i], mini *v[i]);
                     mn = min(mn, v[i]);
                 }
+                // storing the global maximum in the variable
                 gmax = max(gmax, mx);
             }
             return gmax;
