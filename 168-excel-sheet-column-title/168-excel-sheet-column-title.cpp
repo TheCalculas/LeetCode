da303a52b0ec80893e06e54da7d650211c89cb66
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-13 22:37:32 +0530

    Time: 0 ms (100.00%), Space: 6 MB (46.00%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+        string convertToTitle(int colNum)
+        {
+            string ans = "";
+            while (colNum)
+            {
+                colNum--;
+                int r = colNum % 26;
+                char c = 'A' + r;
+                ans = c + ans;
+                colNum /= 26;
+            }
+            return ans;
+        }
+};
