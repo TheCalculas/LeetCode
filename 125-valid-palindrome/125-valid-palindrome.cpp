8cef28c529a4eef4eca5b84309e1b594cc0d1132
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-13 19:56:38 +0530

    Time: 5 ms (74.46%), Space: 7.2 MB (89.73%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,19 +1,32 @@
-class Solution {
-public:
-    bool isPalindrome(string str) {
-        string s = "";
-        for(auto x:str)
-        {   cout<<"first : "<<x<<" ";
-            if(x>='A'&&x<='Z') s.push_back(x+'a'-'A');
-            else if(x>='a'&&x<='z') s.push_back(x);
-            else if(x>='0'&&x<='9') s.push_back(x);
-            else continue;
-            cout<<"baadmei: "<<s[s.size()-1]<<endl;
+class Solution
+{
+    public:
+        bool isPalindrome(string s)
+        {
+            // Bruhh this code is good :)
+            // isalmum checks if the character is alpha numeric or not hmm useful too
+            // and even faster 
+            int left = 0, right = s.size() - 1;
+
+            while (left < right)
+            {
+                if (!isalnum(s[left]))
+                {
+                    left++;
+                    continue;
+                }
+                if (!isalnum(s[right]))
+                {
+                    right--;
+                    continue;
+                }
+                if (tolower(s[left]) != tolower(s[right]))
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
         }
-        cout<<s<<endl;
-        string r = s;
-        reverse(r.begin(), r.end());
-        // if(r.size()==1) return 0;
-        return r==s;
-    }
 };
