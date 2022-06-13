519acc395f33c89a0a11d77439d7834ca022b8e2
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-13 19:08:16 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (85.52%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,24 +1,19 @@
-class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
-        int carry = 1;
-        reverse(digits.begin(), digits.end());
-        for(auto &x: digits)
+class Solution
+{
+    public:
+        vector<int> plusOne(vector<int> &digits)
         {
-            cout<<carry<<" "<<x<<endl;
-            x+=carry;
-            cout<<carry<<" "<<x<<endl;
-            if(x>9) {
-                carry = x-9;
-                x = 0;
+            for (int i = digits.size() - 1; i >= 0; i--)
+            {
+                if (digits[i] != 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                else
+                    digits[i] = 0;
             }
-            else {
-                carry = 0;
-            }
+            digits.insert(digits.begin(), 1);
+            return digits;
         }
-        if(carry)
-            digits.push_back(carry);
-        reverse(digits.begin(), digits.end());
-        return digits;
-    }
 };
