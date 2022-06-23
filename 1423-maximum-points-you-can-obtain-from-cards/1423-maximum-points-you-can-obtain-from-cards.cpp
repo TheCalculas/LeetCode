146cfa35cb34db147cdeb8048b75ac05779499ee
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-24 01:52:21 +0530

    Time: 83 ms (56.74%), Space: 42.3 MB (80.70%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,40 +1,25 @@
-class Solution {
-public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        int n = cardPoints.size();
-        vector<int> prev(n, 0);
-        vector<int> surf(n, 0);
-        prev[0] = cardPoints[0];
-        for(int i=0; i<n-1; i++)
+class Solution
+{
+    public:
+        int maxScore(vector<int> &cardPoints, int k)
         {
-            prev[i+1] = prev[i]+cardPoints[i+1];
+            int sum = 0;
+            int maxsum;
+
+            for (int i = 0; i < k; i++)
+            {
+                sum += cardPoints[i];
+            }
+            maxsum = sum;
+
+            for (int i = 0; i < k; i++)
+            {
+                sum += cardPoints[cardPoints.size() - i - 1] - cardPoints[k - i - 1];
+                if (sum > maxsum)
+                {
+                    maxsum = sum;
+                }
+            }
+            return maxsum;
         }
-        reverse(cardPoints.begin(), cardPoints.end());
-        surf[0] = cardPoints[0];
-        for(int i=0; i<n-1; i++)
-        {
-            surf[i+1] = surf[i]+cardPoints[i+1];
-        }
-        //
-        for(auto x: surf)
-            cout<<x<<" ";
-        cout<<endl;
-        for(auto x: prev)
-            cout<<x<<" ";
-        cout<<endl;
-            
-        int i = 0;
-        int j = k-2;
-        int sum = 0;
-        sum = max(prev[k-1], sum);
-        sum = max(surf[k-1], sum);
-        while(j>=0&&i<=k-2)
-        {
-            sum = max(prev[i]+surf[j], sum);
-            i++;
-            j--;
-        }
-        return sum;
-        
-    }
 };
