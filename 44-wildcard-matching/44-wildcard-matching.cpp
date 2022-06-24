fc167dca3841e484fbb26256a74b8d8162622125
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-24 17:24:06 +0530

    Time: 8 ms (95.51%), Space: 6.6 MB (90.51%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<bool>> dp(s.size() + 1, vector(p.size() + 1, false));
-        dp[0][0] = true;
-        for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
-            dp[0][j + 1] = true;
+        int pi = 0, si = 0, star = -1, count = 0;
+        
+        while (si < s.size()) {
+            
+            if (p[pi] == '*') {
+                count = si;
+                star = pi++;
+            }
+            
+            else if (p[pi] == s[si] || p[pi] == '?') {
+                pi++, si++;
+            }
+            
+            else if (star >= 0){
+                pi = star+1;
+                si = ++count;
+                
+            }
+            else return false;
         }
         
-        for (int i = 1; i <= s.size(); ++i) {
-            for (int j = 1; j <= p.size(); ++j) {
-                if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
-                }
-            }
-        }
-
-        return dp[s.size()][p.size()];
+        while (pi < p.size() && p[pi] == '*') pi++;
+        return pi == p.size();
+        
     }
 };
