bc9cde6208e94d704c442927633b15e7d2a4d54f
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-24 18:41:34 +0530

    Time: 3 ms (63.95%), Space: 7.4 MB (49.95%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        if(nums.size() < 3)
+        if(nums.size() < 3){
             return 0;
-        
-        int cnt = 0, diff = 0 , ind = 0;
-        int prev_diff = nums[1] - nums[0];
-        
-        for(int i = 1; i<nums.size()-1 ; ++i)
-        {
-            // curr difference
-            int diff = nums[i+1] - nums[i];
-            
-            // if we find same diff of consecutive elements
-            // increase count
-            if(diff ==  prev_diff)
-                ++ind;
-            
-            else
-            {
-                // update prev diff with curr diff
-                // as we don't find consecutive elements with same diff
-                prev_diff = diff;
-                ind = 0;  // make ind to 0
-            }
-            
-            // add cosecutive arithmetic sequence cnt
-                cnt += ind;
         }
-        
-       
-        return cnt;
+        for(int i = 1; i < nums.size(); i++){
+            nums[i - 1] = nums[i - 1] - nums[i];
+        }
+        nums[nums.size() - 1] = INT_MIN;
+        int target = nums[0];
+        int count = 0;
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target){
+                count++;
+                continue;
+            }
+            if(count > 1){
+                sum += (count - 1) * count / 2;
+            }
+            count = 1;
+            target = nums[i];
+            
+        }
+        return sum;
     }
 };
