b89cd994309eec6971af0c955959482a97f3fa38
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-06-26 02:36:53 +0530

    Time: 546 ms (15.53%), Space: 25.7 MB (13.10%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,26 +1,33 @@
-class Solution {
-public:
-    int threeSumClosest(vector<int>& nums, int target) {
-    if(nums.size() < 3) return 0;
-    int closest = nums[0]+nums[1]+nums[2];
-    sort(nums.begin(), nums.end());
-    for(int first = 0 ; first < nums.size()-2 ; ++first) {
-        if(first > 0 && nums[first] == nums[first-1]) continue;
-        int second = first+1;
-        int third = nums.size()-1;            
-        while(second < third) {
-            int curSum = nums[first]+nums[second]+nums[third];
-            if(curSum == target) return curSum;
-            if(abs(target-curSum)<abs(target-closest)) {
-                closest = curSum;
+class Solution
+{
+    public:
+        int threeSumClosest(vector<int> &nums, int target)
+        {
+
+            if (nums.size() < 3)
+                return 0;
+           	//Random gauge for sum
+            int sum = nums[0] + nums[1] + nums[2];
+            sort(nums.begin(), nums.end());
+            for (int i = 0; i < nums.size() - 2; i++)
+            {
+                if (i > 0 && nums[i - 1] == nums[i])
+                    continue;
+                int l = i + 1, r = nums.size() - 1;
+                while (l < r)
+                {
+                    int cSum = nums[i] + nums[l] + nums[r];
+                    if (cSum == target)
+                        return cSum;
+                   	//Calculate the difference and find minimal difference
+                    sum = abs(cSum - target) < abs(sum - target) ? cSum : sum;
+                   	//  cout << "\nSum : " << sum  << "\tfrom " << "i: " << i << "\tl: " << l << "\tr:" << r<< endl;
+                    if (cSum < target)
+                        l++;
+                    else if (cSum > target)
+                        r--;
+                }
             }
-            if(curSum > target) {
-                --third;
-            } else {
-                ++second;
-            }
+            return sum;
         }
-    }
-    return closest;
-}
 };
