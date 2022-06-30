e523bd6416054be84cc0b91e492bde70c16f765d
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-07-01 01:53:06 +0530

    Time: 6 ms (41.84%), Space: 6 MB (41.38%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-int dp[31];
-    int helper(int n)
-    {
-        
-        if(n==0) return dp[n] = 0;
-        if(n==1) return dp[n] = 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]  = fib(n-1) + fib(n-2);
-    
-    }
+
     int fib(int n) {
-        memset(dp, -1, sizeof(dp));
-        return helper(n);
+        int dp[n+1];
+        if(n==0) return 0;
+        if(n==1) return 1;
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 0;
+        dp[1] = 1;
+        cout<<dp[0]<<" "<<dp[1]<<endl;
+        for(int i=2; i<n+1; i++)
+        {
+            
+            dp[i] = dp[i-1]+dp[i-2];
+            cout<<"i is : "<<i<<" and dp is "<<dp[i]<<endl;
+        }
+        return dp[n];
     }
 };
