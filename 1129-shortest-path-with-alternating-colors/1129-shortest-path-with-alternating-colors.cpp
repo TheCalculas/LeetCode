ab2836fc9d92966bc46dc78a181a3d161e6e7627
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-07-09 00:43:28 +0530

    Time: 20 ms (90.50%), Space: 15 MB (64.57%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,43 +1,32 @@
-class Solution
-{
-    public:
-        vector<int> shortestAlternatingPaths(int n, vector<vector < int>> &red_edges, vector< vector< int>> &blue_edges)
-        {
-            unordered_map<int, vector < int>> red, blue;
-            for (int i = 0; i < red_edges.size(); i++)
-            {
-                if (red.find(red_edges[i][0]) == red.end()) red[red_edges[i][0]] = {};
-                red[red_edges[i][0]].push_back(red_edges[i][1]);
+
+class Solution {
+public:
+        vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vector<vector<int>>& blue_edges) {
+        vector<vector<pair<int,int>>> g(n);  //index, {neighbor index, color}
+        for(auto& v:red_edges) g[v[0]].push_back({v[1], 0});
+        for(auto& v:blue_edges) g[v[0]].push_back({v[1], 1});
+        vector<vector<int>> vCost(n, vector<int>(2,-1));
+        queue<pair<int,int>> q; // index, color(0 or 1)
+        q.push({0,0});
+        q.push({0,1});
+        vCost[0] = {0,0};
+        while(!q.empty()){
+            auto [i, c1] = q.front(); q.pop();
+            for(auto [j, c2] : g[i]){
+                if(vCost[j][c2] != -1 || c1 == c2) continue;
+                vCost[j][c2] = 1 + vCost[i][c1];
+                q.push({j, c2});
             }
-            for (int i = 0; i < blue_edges.size(); i++)
-            {
-                if (blue.find(blue_edges[i][0]) == blue.end()) blue[blue_edges[i][0]] = {};
-                blue[blue_edges[i][0]].push_back(blue_edges[i][1]);
-            }
-            vector<int> res(n, -1);
-            res[0] = 0;
-            queue<vector < int>> q;
-            q.push({ 0,
-                0,
-                0 });
-            q.push({ 0,
-                1,
-                0 });
-            while (!q.empty())
-            {
-                vector<int> cur = q.front(), neig = red[cur[0]];
-                q.pop();
-                if (cur[1] == 0) neig = blue[cur[0]];
-                for (int i = 0; i < neig.size(); i++)
-                {
-                    if (res[neig[i]] == -1) res[neig[i]] = cur[2] + 1;
-                    q.push({ neig[i],
-                        1 - cur[1],
-                        cur[2] + 1 });
-                }
-                if (cur[1] == 0) blue.erase(cur[0]);
-                else red.erase(cur[0]);
-            }
-            return res;
         }
+        vector<int> res;
+        for (const auto& v : vCost) {
+            int val = v[1];
+            if ((v[0] < val && v[0] != -1) || val == -1) {
+                val = v[0];
+            }
+            res.push_back(val);
+        }
+
+        return res;
+    }
 };
