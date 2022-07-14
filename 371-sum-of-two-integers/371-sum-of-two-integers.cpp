6665a7f0e48b0c651ed9e24c860730ccc03b2320
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-07-14 19:57:10 +0530

    Time: 4 ms (22.66%), Space: 5.9 MB (26.95%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,14 +1,34 @@
-class Solution
-{
-    public:
-        int getSum(int a, int b)
-        {
-            while (b)
-            {
-                unsigned c = a &b;
-                a ^= b;
-                b = c << 1;
+class Solution {
+public:
+    int getSum(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+        
+        // bigger one at first to solve minus op
+        int x = abs(a);
+        int y = abs(b);
+        if (x < y) return getSum(b, a);
+        
+        // result sign, since x > y
+        int sign = a > 0 ? 1 : -1;
+        
+        if (a * b > 0) {
+            // both positive or both negtive
+            while (y) {
+                int rst = x ^ y;
+                int carry = (x & y) << 1;
+                x = rst;
+                y = carry;
             }
-            return a;
+        } else {
+            // one of them is neg, cal x - y since x > y
+            while (y) {
+                int rst = x ^ y;
+                int borrow = (~x & y) << 1;
+                x = rst;
+                y = borrow;
+            }
         }
+        return sign * x;
+    }
 };
