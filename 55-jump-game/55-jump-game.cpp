520a9d2bbee76ba22afabb5276d384113c924292
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-07-16 17:00:39 +0530

    Time: 114 ms (45.95%), Space: 48.5 MB (43.19%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-        bool canJump(vector<int> &nums)
+    bool canJump(vector<int>& nums) {
+        int idx = nums.size() -1;
+        for(int i = nums.size()-1; i>=0; i--)
         {
-            int n = nums.size();
-            vector<bool> dp(n, false);
-            dp[0] = true;
-          
-            for (int i = 1; i < n; i++)
+            if(nums[i]+i>=idx)
             {
-                for (int j = i - 1; j >= 0; j--)
-                {
-                    if (dp[j] && j + nums[j] >= i){
-                        dp[i] = true;
-                        break;
-                    }
-                }
+                idx = i;
             }
-            return dp[n - 1];
         }
+        return idx == 0;
+    }
 };
