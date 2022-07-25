6875283dcac2db42d0f8fe1a67f11d7096f72f5e
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-07-25 12:07:04 +0530

    Time: 18 ms (97.04%), Space: 13.2 MB (92.07%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> G(n);
-        vector<int> degree(n, 0), bfs;
+        vector<vector<int>> G(n); // graph
+        vector<int> degree(n, 0), bfs; // degree
         for (auto& e : prerequisites)
             G[e[1]].push_back(e[0]), degree[e[0]]++;
-        for (int i = 0; i < n; ++i) if (!degree[i]) bfs.push_back(i);
+        for (int i = 0; i < n; ++i) if (!degree[i]) bfs.push_back(i); /// if it is parent 
         for (int i = 0; i < bfs.size(); ++i)
             for (int j: G[bfs[i]])
                 if (--degree[j] == 0) bfs.push_back(j);
-        return bfs.size() == n;
+        return bfs.size() == n; // if all of em gets visited then it is the case
     }
 };
