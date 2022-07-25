1ac58933d80e1fd39f99706638ef1f0fd97f2c46
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-07-25 10:28:35 +0530

    Time: 13 ms (44.01%), Space: 13.1 MB (79.07%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) 
-    {
-        
-        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
-        
-        int m=grid.size();
-        int n=grid[0].size();
-        
+    int orangesRotting(vector<vector<int>>& grid) {
+        int minutes = 0;
         queue<pair<int,int>> q;
-        int fresh=0; //To keep track of all fresh oranges left
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==2)
-                    q.push({i,j});
-                if(grid[i][j]==1)
-                    fresh++;
-            }
-        int ans=-1; //initialised to -1 since after each step we increment the time by 1 and initially all rotten oranges started at 0.
-        while(!q.empty())
-        {
-            int sz=q.size();
-            while(sz--)
-            {
-                pair<int,int> p=q.front();
-                q.pop();
-                for(int i=0;i<4;i++)
-                {
-                    int r=p.first+dir[i];
-                    int c=p.second+dir[i+1];
-                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1)
-                    {
-                        grid[r][c]=2;
-                        q.push({r,c});
-                        fresh--; // decrement by 1 foreach fresh orange that now is rotten
-                    }
-                    
+        for (int i = 0; i<grid.size(); i++){
+            for (int j = 0; j<grid[0].size(); j++){
+                if (grid[i][j]==2){
+                    q.emplace(i,j);
                 }
             }
-            ans++; //incremented after each minute passes
         }
-        if(fresh>0) return -1; //if fresh>0 that means there are fresh oranges left
-        if(ans==-1) return 0; //we initialised with -1, so if there were no oranges it'd take 0 mins.
-        return ans;
-        
+        while (!q.empty()){
+            int a = q.size();
+            while (a--){
+                auto[r,c] = q.front(); q.pop();
+                if (r>0 && grid[r-1][c]==1){
+                    grid[r-1][c] = 2;
+                    q.emplace(r-1,c);
+                }
+                if (c>0 && grid[r][c-1]==1){
+                    grid[r][c-1] = 2;
+                    q.emplace(r,c-1);
+                }
+                if (r<grid.size()-1 && grid[r+1][c]==1){
+                    grid[r+1][c] = 2;
+                    q.emplace(r+1,c);
+                }
+                if (c<grid[0].size()-1 && grid[r][c+1]==1){
+                    grid[r][c+1] = 2;
+                    q.emplace(r,c+1);
+                }
+            }
+            minutes++;
+        }
+        for (int i = 0; i<grid.size(); i++){
+            for (int j = 0; j<grid[0].size(); j++){
+                if (grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        if (minutes) return minutes-1;
+        return minutes;
     }
 };
