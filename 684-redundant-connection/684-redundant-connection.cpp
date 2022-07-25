793a9ec7be0a061a5b5474a8ffa88adddb2294c2
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-07-25 16:45:40 +0530

    Time: 5 ms (92.21%), Space: 8.6 MB (95.39%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,26 @@
+class DSU {
+    vector<int> par, rank;
+public:
+    DSU(int n) : par(n), rank(n) {
+        iota(begin(par), end(par), 0);      
+    }
+    int find(int x) {
+        if(x == par[x]) return x;           
+        return find(par[x]);                
+    }
+    bool Union(int x, int y) {
+        auto xp = find(x), yp = find(y);    
+        if(xp == yp) return false;          
+        return par[xp] = yp;                
+    }
+
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& e) {
+        DSU ds(size(e)+1);
+        for(auto& E : e) 
+            if(!ds.Union(E[0], E[1])) return E;	
+        return { };    
+    }
+};
