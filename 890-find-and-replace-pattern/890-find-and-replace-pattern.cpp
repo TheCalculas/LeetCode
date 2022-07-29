e99254c5a8cee00503fe5c714aec5371231d0e53
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-07-29 22:28:33 +0530

    Time: 7 ms (55.39%), Space: 8.7 MB (29.81%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
-    vector<string> findAndReplacePattern(vector<string>& words, string& pattern) {        
-	vector<string> ans;
-	string p = normalise(pattern);
-	for(auto& w : words) 
-		if(p == normalise(w)) ans.push_back(w);
-	return ans;
-}
-string normalise(string w) {
-	unordered_map<char, char> mp;
-	char c = 'a';
-	for(auto ch : w) 
-		if(!mp.count(ch)) mp[ch] = c++;
-	for(auto& ch : w) ch = mp[ch];
-	return w;
-}
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> ans;
+        for(auto x:words)
+        {
+            if(x.length()==pattern.length())
+            {
+                ans.push_back(x);
+                unordered_map<char,char> mx;
+                unordered_map<char,char> mp;
+                for(int i=0;i<x.length();i++)
+                {
+                    if(mx.find(x[i])==mx.end() && mp.find(pattern[i])!=mp.end())
+                    {
+                        ans.pop_back();
+                        i=x.length();
+                    }
+                    else if(mx.find(x[i])!=mx.end() && mp.find(pattern[i])==mp.end())
+                    {
+                        ans.pop_back();
+                        i=x.length();
+                    }
+                    else if(mx.find(x[i])==mx.end() && mp.find(pattern[i])==mp.end())
+                    {
+                        mx[x[i]]=pattern[i];
+                        mp[pattern[i]]=x[i];
+                    }
+                    else if(mx[x[i]]!=pattern[i] || mp[pattern[i]]!=x[i])
+                    {
+                        ans.pop_back();
+                        i=x.length();
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 };
