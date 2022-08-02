dc29cbd1ee24aaf36c618a3331b88cd1b5f74880
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-08-02 17:08:06 +0530

    Time: 337 ms (47.37%), Space: 75.9 MB (18.38%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,23 +1,34 @@
 class Solution
 {
     public:
-        bool canPartition(vector<int> &nums)
+        bool subsetSumUtil(int ind, int target, vector<int> &arr, vector<vector< int>> &dp)
         {
-            int sum = 0;
-            for (auto a: nums)
-                sum += a;
-            if (sum % 2)
-                return false;
-            sum /= 2;
-            vector<bool> dp(sum + 1, false);
-            dp[0] = true;
-            for (auto a: nums)
-            {
-                for (int i = sum; i >= a; i--)
-                {
-                    dp[i] = dp[i] || dp[i - a];
-                }
-            }
-            return dp[sum];
+            if (target == 0)
+                return true;
+
+            if (ind == 0)
+                return arr[0] == target;
+
+            if (dp[ind][target] != -1)
+                return dp[ind][target];
+
+            bool notTaken = subsetSumUtil(ind - 1, target, arr, dp);
+
+            bool taken = false;
+            if (arr[ind] <= target)
+                taken = subsetSumUtil(ind - 1, target - arr[ind], arr, dp);
+
+            return dp[ind][target] = notTaken || taken;
         }
+    bool canPartition(vector<int> &nums)
+    {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 != 0) return false;
+
+        int n = nums.size();
+        int k = sum / 2;
+        vector<vector < int>> dp(n, vector<int> (k + 1, -1));
+
+        return subsetSumUtil(n - 1, k, nums, dp);
+    }
 };
