b41aa67a3082522c866948f61d47854b633cc305
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-08-05 00:44:46 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (34.29%) - LeetHub

--- 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.cpp
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    string fractionToDecimal(int numerator, int denominator) {
-        if (!numerator) {
-            return "0";
-        }
-        string ans;
-        if (numerator > 0 ^ denominator > 0) {
-            ans += '-';
-        }
-        long n = labs(numerator), d = labs(denominator), r = n % d;
-        ans += to_string(n / d);
-        if (!r) {
-            return ans;
-        }
-        ans += '.';
-        unordered_map<long, int> rs;
-        while (r) {
-            if (rs.find(r) != rs.end()) {
-                ans.insert(rs[r], "(");
-                ans += ')';
-                break;
+    string fractionToDecimal(long long n, long long d) {
+        string ans="";
+        if((n<0&&d>0) || (n>0&&d<0)) ans.push_back('-');
+        n=abs(n);
+        d=abs(d);
+        long long di=n/d;
+        ans+=to_string(di);
+        n%=d;
+        if(n==0) return ans;
+        ans.push_back('.');
+        unordered_map<int,int> mp;
+        while(n){
+            if(mp.find(n)!=mp.end()){
+                ans.insert(ans.begin()+mp[n],'(');
+                ans.push_back(')');
+                return ans;
             }
-            rs[r] = ans.size();
-            r *= 10;
-            ans += to_string(r / d);
-            r %= d;
+            mp[n]=ans.size();
+            n*=10;
+            ans.push_back((n/d)+'0');
+            n%=d;
         }
         return ans;
     }
