fb9af0428716c9358cc61116272b321389fec969
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-08-06 21:43:50 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (58.91%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int poorPigs(int buckets, int poisonTime, int totalTime) {
-    return ceil(log(buckets) / log(totalTime / poisonTime + 1));
+    return ceil(log10(buckets) / log10(totalTime / poisonTime + 1));
 }
 };
