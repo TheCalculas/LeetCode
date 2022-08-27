32b1cc295bf3bd39ca3cf95a377eefda5a766274
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-08-27 16:37:49 +0530

    Time: 312 ms (99.65%), Space: 10.2 MB (100.00%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    int maxSumSubmatrix(vector<vector<int>>& m, int k) {
-    int res = INT_MIN, rows = m.size(), cols = m[0].size();
-    for (int l = 0; l < cols; ++l) {
-        vector<int> sums(rows);
-        for (int r = l; r < cols; ++r) {
-            for (int i = 0; i < rows; ++i) 
-                sums[i] += m[i][r];
-            set<int> s = {0};
-            int run_sum = 0;
-            for (int sum : sums) {
-                run_sum += sum;
-                auto it = s.lower_bound(run_sum - k);
-                if (it != end(s))
-                    res = max(res, run_sum - *it);
-                s.insert(run_sum);
+    int dp[100];
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int sum = 0, ans = -1000000000;
+        for (int i=0; i<n; ++i) {
+            for (int l=0; l<m; ++l) dp[l] = 0;
+            for (int j=i; j<n; ++j){
+                for (int l=0; l<m; ++l) {
+                    dp[l] += matrix[j][l];
+                }
+                
+                for (int a=0; a<m; ++a) {
+                    sum = 0;
+                    for (int b=a; b<m; ++b) {
+                        sum += dp[b]; 
+                        if(sum > ans && sum <= k) ans = sum;
+                    }
+                }
             }
         }
+        
+        return ans;
     }
-    return res;
-}
 };
