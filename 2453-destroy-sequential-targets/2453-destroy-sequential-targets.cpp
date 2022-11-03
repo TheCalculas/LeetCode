a1bb72c79ea188f9a7f895f20952786c988a1558
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-11-03 20:02:20 +0530

    Time: 339 ms (66.25%), Space: 58.8 MB (98.21%) - LeetHub

--- 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -1,38 +1,18 @@
+
 class Solution {
 public:
     int destroyTargets(vector<int>& nums, int space) {
-        unordered_map<int, int> mp, freq;
-        for(auto x: nums)
-        {
-            mp[x]++;
+        int ans = INT_MAX;
+        unordered_map<int, int> mp;
+        int mx = INT_MIN;
+        for(auto n: nums){
+            int r = n % space;                  //evaluate reminder
+            mp[r]++;                               // add reminder to map
+            if(mx < mp[r])  mx = mp[r];   //keep track of the max count, with same reminder
         }
-        unordered_map<int, vector<int>> vv;
-        for(auto x: mp)
-        {
-            int n = x.first;
-            int f = x.second;
-            while(f--)
-            vv[n%space].push_back(n);
+        for(auto n: nums){  //scan smalest element with same riminder as of mx
+            if(mx == mp[n%space])  ans = min(ans, n);
         }
-        int mx = 0;
-        for(auto x: vv)
-        {
-            int a = x.second.size();
-            mx = max(a, mx);
-        }
-        vector<int> ans;
-        for(auto x: vv)
-        {
-            cout<<x.first<<" "<<x.second.size()<<endl;
-            if(x.second.size()==mx)
-            {
-                sort(x.second.begin(), x.second.end());
-                ans.push_back(x.second[0]);
-            }
-        }
-        sort(ans.begin(), ans.end());
-        return ans[0];
-        // return -1;
-        
+        return ans;
     }
 };
