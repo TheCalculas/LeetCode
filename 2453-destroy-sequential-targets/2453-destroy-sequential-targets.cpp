e59d3619046f3530c2bbd2410fc7159f72ab1a0b
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2022-11-03 19:50:03 +0530

    Time: 1283 ms (5.08%), Space: 118.6 MB (5.03%) - LeetHub

---
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int destroyTargets(vector<int>& nums, int space) {
+        map<int, int> mp, freq;
+        for(auto x: nums)
+        {
+            mp[x]++;
+        }
+        map<int, vector<int>> vv;
+        for(auto x: mp)
+        {
+            int n = x.first;
+            int f = x.second;
+            while(f--)
+            vv[n%space].push_back(n);
+        }
+        int mx = 0;
+        for(auto x: vv)
+        {
+            int a = x.second.size();
+            mx = max(a, mx);
+        }
+        vector<int> ans;
+        for(auto x: vv)
+        {
+            cout<<x.first<<" "<<x.second.size()<<endl;
+            if(x.second.size()==mx)
+            {
+                sort(x.second.begin(), x.second.end());
+                ans.push_back(x.second[0]);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans[0];
+        // return -1;
+        
+    }
+};
