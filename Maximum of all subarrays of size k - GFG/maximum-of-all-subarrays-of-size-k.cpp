a1cb6ab2b6503426f72963ad236e04a3d586fee0
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2023-02-05 02:11:07 +0530

    Added solution - LeetHub

--- Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
@@ -14,14 +14,22 @@ class Solution
     //Function to find maximum of each subarray of size k.
     vector <int> max_of_subarrays(int *arr, int n, int k)
     {
+        // implemented deque
          deque<int> dq;
          vector<int> v;
+         
           for (int i = 0; i < n; i++) {
+// if greater element appers then i will pop previous element
             while (!dq.empty() && arr[i] >= arr[dq.back()])
               dq.pop_back();
+            // push toh karna hee hai abhi toh
             dq.push_back(i);
+            // agar k elements ho gaye hai joki init condition thi phele 
             if (i >= k - 1) {
+            // abh dekh first element is the largest toh vahi ans hoga
               v.push_back(arr[dq.front()]);
+              // iska matalbh hai ki basically iska time ho chuka hai 
+              // toh ise pop kardo abh koi kaam nhi hai iska 
               if (dq.front() <= i - k + 1)
                 dq.pop_front();
                 }
