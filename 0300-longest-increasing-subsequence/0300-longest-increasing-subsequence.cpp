160ba9efc2d53c78bd345450d2ea8cd2bef62d29
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2023-06-09 18:34:24 +0530

    Time: 3 ms (99.65%), Space: 10.3 MB (85.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,15 @@
-class Solution {
+class Solution { // 8 ms, faster than 91.61%
 public:
-    int f(vector<int>& v, int idx, int pre_idx, vector<vector<int>>&dp)
-    {
-        if(idx==v.size()+1) return 0;
-        if(dp[idx][pre_idx]!=-1) return dp[idx][pre_idx];
-        int len = 0 + f(v, idx+1, pre_idx, dp);
-        if(pre_idx==0||v[idx-1]>v[pre_idx-1])
-        {
-            return dp[idx][pre_idx] = max(len, 1 + f(v, idx+1, idx, dp));
-        }
-        return dp[idx][pre_idx] = len;
-    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size()+2, vector<int>(nums.size()+2, -1));
-        return f(nums, 1, 0, dp);
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the first element >= x
+                *it = x; // Replace that number with x
+            }
+        }
+        return sub.size();
     }
 };
