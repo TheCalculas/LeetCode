6b141e98dbfedb7a3003496a25c7f9569b952d49
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2023-06-22 15:26:24 +0530

    Time: 262 ms (55.32%), Space: 84.6 MB (86.70%) - LeetHub

---
+++ 2561-rearranging-fruits/2561-rearranging-fruits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long minCost(vector<int>& b1, vector<int>& b2) {
+        map<int, long long> m;
+        for (auto c : b1)
+            ++m[c];
+        for (auto c : b2)
+            --m[c];
+        long long swaps = 0, res = 0;
+        for (auto [c, cnt] : m) {
+            if (cnt % 2)
+                return -1;
+            swaps += max(0LL, cnt / 2);
+        }
+        for (auto [c, cnt] : m) {
+            long long take = min(swaps, abs(cnt) / 2);
+            res += take * min(c, begin(m)->first * 2);
+            swaps -= take;
+        }
+        return res;
+    }
+};
