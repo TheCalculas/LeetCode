545653cf96ce5836887dde6360134c81a0549fb1
Dharmesh Sharma <77923808+TheCalculas@users.noreply.github.com>
2023-06-22 14:53:47 +0530

    Time: 102 ms (96.86%), Space: 55 MB (77.58%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-    int f(vector<int>&p, int fee, int idx, int buy, vector<vector<int>>&dp)
-    {
-        if(idx>=p.size())
-        {
-            return 0;
-        }
-        
-        // take 
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        int b = 0, s = 0;
-        int notSell = f(p, fee, idx+1, buy, dp);
-        b = notSell;
-        s = notSell;
-        if(buy==1)
-        {
-            // if already bought 
-            int sell = f(p, fee, idx+1, 0, dp) + p[idx]- fee;
-            
-            b = max(sell, b);
-        }
-        else{
-            int bu = f(p, fee, idx+1, 1, dp) - p[idx];
-            
-            s = max(bu, s);
-        }
-        
-        return dp[idx][buy] = max(b, s);
-        
-    }
     int maxProfit(vector<int>& prices, int fee) {
-        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
-        return f(prices, fee, 0, 0, dp);
+        int buy = 0, notBuy = 0;
+        for(int i=prices.size()-1;i>=0;i--){
+            int curBuy , curNotBuy ;
+            curBuy = max(-prices[i] + notBuy, buy);
+            curNotBuy = max(prices[i] - fee + buy, notBuy);
+
+            buy = curBuy;
+            notBuy = curNotBuy;
+        }
+
+        return buy;
     }
 };
